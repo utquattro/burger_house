f9c7369b9cc412d64d23904f6333a65860331218
wnpip <utquattro@gmail.com>
2023-06-20 20:06:18 +0300

    добавил null в discount_price, поле weight и photo_url обязательны

--- menu/models.py
+++ menu/models.py
@@ -6,14 +6,15 @@ class Menu(models.Model):
     """"""
     title = models.TextField(max_length=250, blank=False)
     description = models.TextField(max_length=500, blank=True)
-    weight = models.IntegerField(blank=True, validators=[MinValueValidator(1)])
+    weight = models.IntegerField(blank=False, validators=[MinValueValidator(1)])
     price = models.IntegerField(blank=False, validators=[MinValueValidator(1)])
-    discount_price = models.IntegerField(blank=True, validators=[MinValueValidator(1)])
-    photo_url = models.ImageField(upload_to='images')
+    discount_price = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1)])
+    photo_url = models.ImageField(blank=False, upload_to='images',)
     add_date = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True, blank=True)
 
     def __str__(self):
         return self.title
+
 
 
